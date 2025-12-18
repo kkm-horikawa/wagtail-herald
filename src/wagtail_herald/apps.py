43d9736670fea2a3b513627c4b72b875d9266c60
karrinn <91161396+kkm-horikawa@users.noreply.github.com>
2025-12-18 11:31:20 +0900

    feat: add i18n support with Japanese locale (#24)
    * feat: initialize i18n setup
    
    Track progress for #15
    
    * feat: add i18n support with Japanese locale
    
    - Add gettext_lazy import to apps.py for translatable verbose_name
    - Create locale directory structure for en and ja
    - Generate .po files with django-admin makemessages
    - Add Japanese translations for all model fields and panels
    - Compile .mo files for runtime use
    
    Github-Issue: #15

--- src/wagtail_herald/apps.py
+++ src/wagtail_herald/apps.py
@@ -3,6 +3,7 @@ Django app configuration for wagtail-herald.
 """
 
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class WagtailHeraldConfig(AppConfig):
@@ -10,5 +11,5 @@ class WagtailHeraldConfig(AppConfig):
 
     name = "wagtail_herald"
     label = "wagtail_herald"
-    verbose_name = "Wagtail Herald"
+    verbose_name = _("Wagtail Herald")
     default_auto_field = "django.db.models.BigAutoField"
